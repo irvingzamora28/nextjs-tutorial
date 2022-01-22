ea60ae654593360065ae1fad574b0173a2a51875
Irving Zamora <irvingzamora28@gmail.com>
2022-01-22 14:38:46 -0700

    Add layout

---
+++ components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavStyles from '../styles/Nav.module.css'
+
+const Nav = () => {
+    return (
+        <div className={NavStyles.nav}>
+            
+        </div>
+    )
+}
+
+export default Nav
---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import React from 'react'
+
+const about = () => {
+    return (
+        <div>
+            <Head>
+                <title>About page</title>
+				<meta name="description" content="About page" />
+            </Head>
+            About
+        </div>
+    )
+}
+
+export default about
